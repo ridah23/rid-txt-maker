873e28487ea54a594c5e6d4a86717d7e4370cbcf
ridah23 <101697619+ridah23@users.noreply.github.com>
2022-03-17 06:06:11 +0700

    ktu we

--- photooxy-examples/photooxy-ex.js
+++ photooxy-examples/photooxy-ex.js
@@ -1,9 +1,9 @@
-const yasiapi = require('textmaker-yasii'); // Import NPM Package
+const rid-api = require('rid-txt-maker'); // Import NPM Package
 
 // Some Examples
 
-yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/8-bit-text-on-arcade-rift-175.html",
-    ["Developer"], ["Sl-Yasia"]
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/8-bit-text-on-arcade-rift-175.html",
+    ["Developer"], ["RID"]
     ).then(async (data) => { 
       try { 
           console.log(data)
@@ -11,8 +11,8 @@ yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/8-bit-text-on-arcad
           console.log(err)
       } 
 });
-yasiapi.photooxy("https://photooxy.com/battlegrounds/make-wallpaper-battlegrounds-logo-text-146.html",
-    ["Developer"], ["Sl-Yasia"]
+rid-api.photooxy("https://photooxy.com/battlegrounds/make-wallpaper-battlegrounds-logo-text-146.html",
+    ["Developer"], ["RID"]
     ).then(async (data) => { 
       try { 
           console.log(data)
@@ -20,8 +20,8 @@ yasiapi.photooxy("https://photooxy.com/battlegrounds/make-wallpaper-battleground
           console.log(err)
       } 
 });
-yasiapi.photooxy("https://photooxy.com/fps-game-effect/create-battlefield-4-rising-effect-152.html",
-    ["Developer"], ["Sl-Yasia"]
+rid-api.photooxy("https://photooxy.com/fps-game-effect/create-battlefield-4-rising-effect-152.html",
+    ["Developer"], ["RID"]
     ).then(async (data) => { 
       try { 
           console.log(data)
@@ -29,8 +29,8 @@ yasiapi.photooxy("https://photooxy.com/fps-game-effect/create-battlefield-4-risi
           console.log(err)
       } 
 });
-yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/text-on-scary-cemetery-gate-172.html",
-    "Sl-Yasia"
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/text-on-scary-cemetery-gate-172.html",
+    "RID"
     ).then(async (data) => { 
       try { 
           console.log(data)
@@ -38,8 +38,8 @@ yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/text-on-scary-cemet
           console.log(err)
       } 
 });
-yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/text-under-flower-165.html",
-    "Sl-Yasia"
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/text-under-flower-165.html",
+    "RID"
     ).then(async (data) => { 
       try { 
           console.log(data)
@@ -47,8 +47,8 @@ yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/text-under-flower-1
           console.log(err)
       } 
 });
-yasiapi.photooxy("https://photooxy.com/other-design/create-metallic-text-glow-online-188.html",
-    "Sl-Yasia"
+rid-api.photooxy("https://photooxy.com/other-design/create-metallic-text-glow-online-188.html",
+    "RID"
     ).then(async (data) => { 
       try { 
           console.log(data)
@@ -56,8 +56,8 @@ yasiapi.photooxy("https://photooxy.com/other-design/create-metallic-text-glow-on
           console.log(err)
       } 
 });
-yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/realistic-flaming-text-effect-online-197.html",
-    "Sl-Yasia"
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/realistic-flaming-text-effect-online-197.html",
+    "RID"
     ).then(async (data) => { 
       try { 
           console.log(data)
@@ -65,8 +65,8 @@ yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/realistic-flaming-t
           console.log(err)
       } 
 });
-yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/make-smoky-neon-glow-effect-343.html",
-    "Sl-Yasia"
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/make-smoky-neon-glow-effect-343.html",
+    "RID"
     ).then(async (data) => { 
       try { 
           console.log(data)
@@ -74,8 +74,8 @@ yasiapi.photooxy("https://photooxy.com/logo-and-text-effects/make-smoky-neon-glo
           console.log(err)
       } 
 });
-yasiapi.photooxy("https://photooxy.com/other-design/create-an-easy-smoke-type-effect-390.html",
-    "Sl-Yasia"
+rid-api.photooxy("https://photooxy.com/other-design/create-an-easy-smoke-type-effect-390.html",
+    "RID"
     ).then(async (data) => { 
       try { 
           console.log(data)
