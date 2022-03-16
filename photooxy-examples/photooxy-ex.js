db018981fd45c370d3239aa4ffb5a8ecaab0cd98
Ky <razahra835@gmail.com>
2022-03-17 06:21:41 +0700

    initial changes

---
+++ photooxy-examples/photooxy-ex.js
@@ -0,0 +1,89 @@
+const rid-api = require('rid-txt-maker'); // Import NPM Package
+
+// Some Examples
+
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/8-bit-text-on-arcade-rift-175.html",
+    ["Developer"], ["RID"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+rid-api.photooxy("https://photooxy.com/battlegrounds/make-wallpaper-battlegrounds-logo-text-146.html",
+    ["Developer"], ["RID"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+rid-api.photooxy("https://photooxy.com/fps-game-effect/create-battlefield-4-rising-effect-152.html",
+    ["Developer"], ["RID"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/text-on-scary-cemetery-gate-172.html",
+    "RID"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/text-under-flower-165.html",
+    "RID"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+rid-api.photooxy("https://photooxy.com/other-design/create-metallic-text-glow-online-188.html",
+    "RID"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/realistic-flaming-text-effect-online-197.html",
+    "RID"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+rid-api.photooxy("https://photooxy.com/logo-and-text-effects/make-smoky-neon-glow-effect-343.html",
+    "RID"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+rid-api.photooxy("https://photooxy.com/other-design/create-an-easy-smoke-type-effect-390.html",
+    "RID"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+
+/*
+ And Usually all of the others support :)
+*/
